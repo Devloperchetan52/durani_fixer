d8ef85a95cbaac0748e07f918f830e1cdc51d21a
Devloperchetan52 <106262071+Devloperchetan52@users.noreply.github.com>
2025-03-28 12:10:51 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ function App() {
       <div className="box">
         <div class="wrap">
           <div class="content content1">
-            <p class="blink">⭕ 𝟭𝟬𝟬% 𝗔𝗖𝗖𝗨𝗥𝗔𝗖𝗬 𝗜𝗡 𝗜𝗣𝗟 𝟮𝟬𝟮𝟰 ⭕</p>
+            <p class="blink">⭕ 𝟭𝟬𝟬% 𝗔𝗖𝗖𝗨𝗥𝗔𝗖𝗬 𝗜𝗡 𝗜𝗣𝗟 𝟮𝟬𝟮𝟓 𝐓𝐈𝐋𝐋 𝐍𝐎𝐖 ⭕</p>
           </div>
         </div>
         <button class="custom-btn btn-11">JOIN TELEGRAM</button>
@@ -37,8 +37,8 @@ function App() {
             👑 𝗞𝗜𝗡𝗚 𝗞𝗢𝗛𝗟𝗜'𝗦 𝗕𝗘𝗦𝗧 𝗙𝗥𝗜𝗘𝗡𝗗 👑
           </p>
         </div>
-        <p className="detail">⭕ 𝗜𝗣𝗟 𝟮𝟬𝟮𝟯 - 𝟳𝟮/𝟳𝟰 𝗣𝗔𝗦𝗦✅ ⭕ </p>
-        <p className="detail">⭕ 𝗙𝗨𝗟𝗟 𝗙𝗜𝗫𝗘𝗗 𝗜𝗣𝗟 𝟮𝟬𝟮𝟰 ⭕</p>
+        <p className="detail">⭕𝗜𝗣𝗟 𝟮𝟬𝟮𝟒 - 𝟳𝟮/𝟳𝟰 𝗣𝗔𝗦𝗦✅ ⭕ </p>
+        <p className="detail">⭕ 𝗙𝗨𝗟𝗟 𝗙𝗜𝗫𝗘𝗗 𝗜𝗣𝗟 𝟮𝟬𝟮𝟓 ⭕</p>
         <p className="detail">⭕ 𝗔𝗟𝗟 𝗧𝗢𝗦𝗦 + 𝗠𝗔𝗧𝗖𝗛 + 𝗦𝗘𝗦𝗦𝗜𝗢𝗡 ⭕</p>
         <p className="detail">⭕ 𝗣𝗨𝗡𝗧𝗘𝗥 𝗙𝗥𝗜𝗘𝗡𝗗𝗟𝗬 𝗖𝗛𝗔𝗡𝗡𝗘𝗟 ⭕</p>
         <div className="item button-pulse">
